1fcdf9fb79e05ab75598149075fb3c8668cd72ed
Juan Ortiz <juan@JuanElias-MacBook-Pro.local>
2025-02-18 11:09:24 -0400

    Starting game loop, loading display

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import pygame
-import constants
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    pygame.init()
 
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill('black')
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
